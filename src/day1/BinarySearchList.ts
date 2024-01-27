54f3587f376c9371f99ce1ac9edf0f0dc656ee87
Dewa made <108676698+DewaMadeWira@users.noreply.github.com>
2024-01-27 10:03:30 +0700

    binary search list

---
+++ src/day1/BinarySearchList.ts
@@ -0,0 +1,18 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let hi = haystack.length;
+
+    do {
+        let m = Math.floor(low + (hi - low) / 2);
+        let v = haystack[m];
+
+        if (v === needle) {
+            return true;
+        } else if (v > needle) {
+            hi = m;
+        } else {
+            low = m + 1;
+        }
+    } while (low < hi);
+    return false;
+}
