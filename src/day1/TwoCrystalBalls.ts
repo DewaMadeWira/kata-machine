0a37cceb5806b9c3917b7f4759b22e6b6b6eb74f
Dewa made <108676698+DewaMadeWira@users.noreply.github.com>
2024-02-01 11:24:50 +0700

    crystal ball modified

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,23 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+    let i = jumpAmount;
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i] == true) {
+            break;
+        }
+    }
+    let j = i - jumpAmount;
+    // for (let j = 0; j <= jumpAmount && j < breaks.length; ++j, ++i) {
+    //     if (breaks[i] == true) {
+    //         return i;
+    //     }
+    // }
 
+    for (j; j <= jumpAmount && j < breaks.length; j++) {
+        if (breaks[j] == true) {
+            return i;
+        }
+    }
+
+    return -1;
 }
